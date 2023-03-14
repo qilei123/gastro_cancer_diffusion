4fecf58bca59fb2ba70943ff60ba8d39b8546308
qilei123 <qileimail123@gmail.com>
2023-03-14 17:57:39 -0400

    more exp

--- train_cancer_diffusion.py
+++ train_cancer_diffusion.py
@@ -19,7 +19,7 @@ class TrainingConfig:
     save_image_epochs = 10
     save_model_epochs = 30
     mixed_precision = 'fp16'  # `no` for float32, `fp16` for automatic mixed precision
-    output_dir = 'output/gastro_images_mask_data1_256_n700_crop_be1'  # the model namy locally and on the HF Hub
+    output_dir = 'output/gastro_images_mask_data2_256_n700_crop_be1'  # the model namy locally and on the HF Hub
 
     push_to_hub = False  # whether to upload the saved model to the HF Hub
     hub_private_repo = False  
@@ -38,7 +38,7 @@ class TrainingConfig:
     
     bbox_extend = 1
     
-    dataset_name='dataset1'
+    dataset_name='dataset2'
     
     def __str__(self) -> str:
         pass
